8212261ad02a80006ab9dd8a03d84a8e4b0045ba
kantlori <atabeywade@gmail.com>
2025-06-13 14:42:40 +0300

    feat: add Tailwind CSS support and configure PostCSS

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import tailwindcss from 'tailwindcss'
+import tailwindcssPlugin from '@tailwindcss/postcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcssPlugin,
+        autoprefixer,
+      ],
+    },
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
